652d75fd7086321011f43da29adf430e0f3ee1b0
Fernando Mercês <nandu88@gmail.com>
2017-04-13 23:13:11 -0400

    Fix: aula07

--- aula07/oplogicos.c
+++ aula07/oplogicos.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 /*
+ * Conjuncao (E, AND)
+ * 0 0 - Falso - 0
+ * 0 1 - Falso - 0
+ * 1 0 - Falso - 0
+ * 1 1 - Verdadeiro - 1
+ *
  * Disjuncao (OU, OR)
  * 0 0 - Falso - 0
  * 0 1 - Verdadeiro - 1
@@ -8,15 +14,13 @@
  * 1 1 - Verdadeiro - 1
 */
 
-/* >, <, ==, !=, >=, <=
+// >, <, ==, !=, >=, <=
 
 int main(void) {
   int a = 8;
   int b = 8;
 
   printf("O resultado eh %d\n", !(!(32 > 8) && 3 == 9) );
-  
-
 
   return 0;
 }
